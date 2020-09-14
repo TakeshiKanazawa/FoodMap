f1ffa50d034fff61dda0dc98cbf93190089e5f73
Takeshi Kanazawa <oseans@hotmail.co.jp>
2020-09-14 20:58:00 +0900

    Revert "不要なコメント削除"
    This reverts commit 7d0480df6819325c350d50f371fb9f5eed6de920.

--- FoodMap/View/MapXibView.swift
+++ FoodMap/View/MapXibView.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class MapXibView: UIView {
 
+//    override init(frame: CGRect){
+//          super.init(frame: frame)
+//          loadNib()
+//      }
+//
+//      required init(coder aDecoder: NSCoder) {
+//          super.init(coder: aDecoder)!
+//          loadNib()
+//      }
 
       func loadNib(){
           let view = Bundle.main.loadNibNamed("MapViewController", owner: self, options: nil)?.first as! UIView
